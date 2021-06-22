96896fbba925338f0e983bcc7318841b17597852
Fernando Silva <sktnando@gmail.com>
2021-06-22 18:18:34 -0300

    estoque sendo mostrado apenas quando tem assinatura ativa

--- src/components/pages/Stock/Stock.vue
+++ src/components/pages/Stock/Stock.vue
@@ -1,13 +1,23 @@
 <template>
     <b-card no-body>
-        <b-tabs card>
-            <b-tab title="À venda" active @click="listaEmEstoque()">
-                <InStock/>
-            </b-tab>
-            <b-tab title="Vendidos" @click="listaVendidos()">
-                <SoldOut/>
-            </b-tab>
+        <b-tabs card v-if="(subscription.status == 2 || subscription.status == 0)">
+            <div class="px-2 pt-1" v-if="loadingSubscription">
+                <b-skeleton animation="fade" height="200px" class="rounded"></b-skeleton>
+            </div>
+            <template v-else>
+                <b-tab title="À venda" active @click="listaEmEstoque()">
+                    <InStock/>
+                </b-tab>
+                <b-tab title="Vendidos" @click="listaVendidos()">
+                    <SoldOut/>
+                </b-tab>
+            </template>            
         </b-tabs>
+        <template v-else>
+            <b-alert show variant="danger" class="mx-1 my-1 text-center">
+                Não encontramos nenhuma assinatura ativa :( se deseja gerenciar sua assinatura, clique <router-link :to="'/subscription'">aqui</router-link>
+            </b-alert>
+        </template>
     </b-card>
 </template>
 
@@ -19,10 +29,34 @@ export default {
         InStock,
         SoldOut,
     },
-    created() {
+    data() {
+        return {
+            loadingSubscription: false,
+        }
+    },    
+    async created() {
+        await this.validateSubscription()
         this.listaEmEstoque()
     },
+    computed: {
+        subscription: {
+            get() {
+                return this.$store.getters['subscription/userSubscription']
+            },
+            set(val) {
+                this.$store.commit('subscription/userSubscription', val)
+            }
+        },
+    },    
     methods: {
+        async validateSubscription() {
+            this.loadingSubscription = true
+            let {
+                data: subscription
+            } = await this.$store.dispatch('subscription/validateSubscription')
+            this.subscription = subscription
+            this.loadingSubscription = false
+        },        
         async listaEmEstoque() {
             this.$store.dispatch('products/listaProdutosEmEstoque')
         },
